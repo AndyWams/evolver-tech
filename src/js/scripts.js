7c7437512211ab3f9c5f57ba6cceae39f9c0a7e3
wams <andrew@lightbulbux.com>
2020-10-09 23:55:36 +0100

    update

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,1 +1,12 @@
-$(function () {});
+$(function () {
+  $(document).ready(() => {
+    $(".scroll").on("click", () => {
+      $("html, body").animate(
+        {
+          scrollTop: $(".tier_one").offset().top,
+        },
+        1000
+      );
+    });
+  });
+});

1594565369fa070bab004bf63f001b0a3ec854b2
wams <andrew@lightbulbux.com>
2020-10-10 13:02:47 +0100

    update

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -8,5 +8,14 @@ $(function () {
         1000
       );
     });
+    $(".mb-view ion-icon, .sidenav .close_").click(function () {
+      $(".sidenav").toggleClass("show");
+    });
+    $(".controls .next").click(function () {
+      $(".tier_two .list-items").animate({ scrollLeft: "+=460" }, 1000);
+    });
+    $(".controls .prev").click(function () {
+      $(".tier_two .list-items").animate({ scrollLeft: "-=460" }, 1000);
+    });
   });
 });
